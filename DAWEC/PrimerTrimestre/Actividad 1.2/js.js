29900c3292dcbdc2f6058a50c1abdb1aa419d7b5
GabinTC <gabintella@gmail.com>
2024-09-23 10:15:43 +0200

    Actividad 1.2 dawec acabada

---
+++ DAWEC/PrimerTrimestre/Actividad 1.2/js.js
@@ -0,0 +1,1 @@
+alert("boeee asi esta el javascript????");
---
+++ DAWEC/PrimerTrimestre/Actividad 1.2/texto.js
@@ -0,0 +1,3 @@
+document.write("<a href='https://www.lingscars.com/'>Esto ha sido escrito desde un documento externo</a>");
+
+document.getElementById("tit2").innerHTML = "titulo con inner html";
